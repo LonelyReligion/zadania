eaf4a4efea3d289c7401ba4fdfd0da20214bab66
flxgttr <agatazbirek@outlook.com>
2024-05-16 12:28:53 +0200

    ef integration

--- TABProject/Program.cs
+++ TABProject/Program.cs
@@ -12,11 +12,40 @@ namespace TABProject
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginView());
+            using (var db = new TABContext())
+            {
+                var user = new app_user {type="type",  password="admin", login="admin", active=true};
+                db.app_user.Add(user);
+                db.SaveChanges();
+
+                var query = from b in db.app_user
+                            orderby b.login
+                            select b;
+                Console.WriteLine("Wszystkie loginy w bazie:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.login);
+                };
+
+                //sprzatamy po sobie 
+                db.app_user.Remove(user);
+                db.SaveChanges();
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new LoginView());
+            }
         }
     }
 }
+
+        
+            
+
+
+            
+
+       
+        
